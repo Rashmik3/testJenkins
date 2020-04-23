f24f7c6ed3915d3537f07f431458bc2c9abf9cbd
rashmik3 <kumari.rashmi06071993@gmail.com>
2020-04-23 18:00:50 +0200

    test

--- src/main/java/com/rashmi/assignment/anagrams/Anagrams.java
+++ src/main/java/com/rashmi/assignment/anagrams/Anagrams.java
@@ -12,29 +12,17 @@ public class Anagrams {
 		String a= "care1";
 		String b= "race1";
 		if ( a.length() == b.length()) {
-							a  = a.replaceAll("\\s", "").toLowerCase();
-					        b = b.replaceAll("\\s", "").toLowerCase();
-					        // convert string to char array
-					        char[] firstArray = a.toCharArray();
-					        char[] secondArray = b.toCharArray();
-					        // sort both the arrays
-					        Arrays.sort(firstArray);
-					        Arrays.sort(secondArray);
-					        
-					        if (Arrays.equals(firstArray, secondArray)) {
-					        	System.out.println("anagram");
-						}
-		
-		
-				}
-				
-	
-			
-	
-		
-		
-		
-		
+			a = a.replaceAll("\\s", "").toLowerCase();
+			b = b.replaceAll("\\s", "").toLowerCase();
+			// convert string to char array
+			char[] firstArray = a.toCharArray();
+			char[] secondArray = b.toCharArray();
+			// sort both the arrays
+			Arrays.sort(firstArray);
+			Arrays.sort(secondArray);
+			if (Arrays.equals(firstArray, secondArray)) {
+				System.out.println("anagram");
+			}
+		}
 	}
-
 }
